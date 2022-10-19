76f5701743cf6884b010d1fe36c8718f23ee71b1
Jignasu <1jignasu@gmail.com>
2022-10-19 17:44:00 -0400

    Cleared an error in server.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 
-const path = required('path');
+const path = require('path');
 
 const port = process.env.PORT || 3000;
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static('build'));
+    app.get('*', (req, res)=>{
+        req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    })
+}
 
 app.listen(port, '0.0.0.0', () => console.log(`Server running on port ${port}`));
