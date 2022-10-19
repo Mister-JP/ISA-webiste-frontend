c71d0b1b6e390935c743dc40200732484bb4a646
Jignasu <1jignasu@gmail.com>
2022-10-19 19:11:55 -0400

    Debugged the server.js for deployement

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const app = express();
 const path = require('path');
 
 const port = process.env.PORT || 3000;
-
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static('build'));
-    app.get('*', (req, res)=>{
-        req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static(path.join(__dirname,'build')));
+    app.get('*', async (req, res)=>{
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
     })
 }
 
-app.listen(port, '0.0.0.0', () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
